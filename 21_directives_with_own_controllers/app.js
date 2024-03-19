df0694ac4f435d00b499421f659f881bcea1cb8f
jonathan_fambro <startek591@gmail.com>
2024-03-19 15:12:14 -0400

    Directives with Their Own Controllers

---
+++ 21_directives_with_own_controllers/app.js
@@ -0,0 +1,96 @@
+(function () {
+  angular
+    .module('ShoppingListDirectiveApp', [])
+    .controller('ShoppingListController', ShoppingListController)
+    .factory('ShoppingListFactory', ShoppingListFactory)
+    .directive('shoppingList', ShoppingListDirective);
+
+  function ShoppingListDirective() {
+    const ddo = {
+      templateUrl: 'shoppingList.html',
+      scope: {
+        items: '<',
+        title: '@',
+      },
+      controller: ShoppingListDirectiveController,
+      controllerAs: 'list',
+      bindToController: true,
+    };
+
+    return ddo;
+  }
+
+  function ShoppingListDirectiveController() {
+    let list = this;
+
+    list.cookiesInList = function () {
+      for (let i = 0; i < list.items.length; i++) {
+        let name = list.items[i].name;
+        if (name.toLowerCase().indexOf('cookie') !== -1) {
+          return true;
+        }
+      }
+      return false;
+    };
+  }
+
+  ShoppingListController.$inject = ['ShoppingListFactory'];
+  function ShoppingListController(ShoppingListFactory) {
+    let list = this;
+    let shoppingList = ShoppingListFactory();
+
+    list.items = shoppingList.getItems();
+    let origTitle = 'Shopping List #1';
+    list.title = origTitle + ' (' + list.items.length + ' items )';
+
+    list.itemName = '';
+    list.itemQuantity = '';
+
+    list.addItem = function () {
+      shoppingList.addItem(list.itemName, list.itemQuantity);
+      list.title = origTitle + ' (' + list.items.length + ' items )';
+    };
+
+    list.removeItem = function (itemIndex) {
+      shoppingList.removeItem(itemIndex);
+      list.title = origTitle + ' (' + list.items.length + ' items )';
+    };
+  }
+
+  function ShoppingListService(maxItems) {
+    let service = this;
+
+    const items = [];
+
+    service.addItem = function (itemName, quantity) {
+      if (
+        maxItems === undefined ||
+        (maxItems !== undefined && items.length < maxItems)
+      ) {
+        const item = {
+          name: itemName,
+          quantity: quantity,
+        };
+        items.push(item);
+      } else {
+        throw new Error('Max items (' + maxItems + ') reached.');
+      }
+    };
+
+    service.removeItem = function (itemIndex) {
+      items.splice(itemIndex, 1);
+    };
+
+    service.getItems = function () {
+      return items;
+    };
+  }
+
+  function ShoppingListFactory() {
+    const factory = function (maxItem) {
+      return new ShoppingListService(maxItem);
+    };
+
+    return factory;
+  }
+})();
